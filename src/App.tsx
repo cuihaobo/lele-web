379a32d09943218296cd977a32708d56294878af
cuihoabo <Lelecui123@163.com>
2023-06-12 19:36:34 +0800

    testing2023612

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css'
-import { TopNavigation } from './components/TopNavigation'
-import { RouterMap } from './RouterMap'
+import { Layout } from "antd";
+import "./App.css";
+import { TopNavigation } from "./components/TopNavigation";
+import { RouterMap } from "./RouterMap";
+import { Content, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 
 function App() {
-
   return (
     <>
-        <TopNavigation/>
-        <RouterMap />
+      <TopNavigation />
+      <Layout style={{ height: "100vh" }}>
+        <Sider></Sider>
+        <Content>
+          <RouterMap />
+        </Content>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import { Col, Row } from "antd"
+
 export const Home = () => {
     return (
-        <h1>Home</h1>
+        <Row>
+      <Col  sm={4} md={6}  xl={10} span={6}>col-6</Col>
+      <Col  sm={4} md={6}  xl={10} span={6}>col-6</Col>
+      <Col  sm={4} md={6}  xl={10} span={6}>col-6</Col>
+      <Col  sm={4} md={6}  xl={10} span={6}>col-6</Col>
+    </Row>
     )
 }
