e75dcd360c6168ee541d9ec7a77a07055ce2928a
David M. Chen <chen7david@me.com>
2023-07-31 09:17:09 -0300

    added timeline to profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,22 @@
+import { ClockCircleOutlined } from "@ant-design/icons"
+import { Timeline } from "antd"
+
+
 export const Profile = () => {
+    const item = {
+        children: 'Lele\'s birthday.',
+
+        dot: <ClockCircleOutlined />
+    }
+    const items = [
+        item,
+        item,
+        item,
+        item,
+    ]
     return (
-        <h1>Profile</h1>
+        <Timeline
+            items={items}
+        />
     )
 }
